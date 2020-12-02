ad351b39865c4ddc30bf6f9ec8baa333f141676a
njuro <juraj.noge@gmail.com>
2020-12-02 06:30:42 +0100

    Day 2

---
+++ src/main/kotlin/day2.kt
@@ -0,0 +1,23 @@
+import utils.readInputLines
+
+/** [https://adventofcode.com/2020/day/2] */
+class Passwords : AdventOfCodeTask {
+    override fun run(part2: Boolean): Any {
+        val pattern = Regex("(\\d+)-(\\d+) (\\w): (\\w+)")
+        return readInputLines("2.txt").map { pattern.matchEntire(it) }.count {
+            val (min, max, letter, password) = it!!.destructured
+            if (part2) {
+                val first = password[min.toInt() - 1].toString()
+                val second = password[max.toInt() - 1].toString()
+                first != second && (first == letter || second == letter)
+            } else {
+                val count = password.count { c -> c == letter[0] }
+                count >= min.toInt() && count <= max.toInt()
+            }
+        }
+    }
+}
+
+fun main() {
+    println(Passwords().run(part2 = true))
+}
--- src/test/kotlin/AdventOfCodeTasksTest.kt
+++ src/test/kotlin/AdventOfCodeTasksTest.kt
@@ -8,6 +8,11 @@ class AdventOfCodeTasksTest {
         runTaskTest(FindSum(), 996996, 9210402)
     }
 
+    @Test
+    fun day2() {
+        runTaskTest(Passwords(), 582, 729)
+    }
+
     private fun runTaskTest(task: AdventOfCodeTask, part1Result: Any, part2Result: Any) {
         assertEquals(part1Result, task.run())
         assertEquals(part2Result, task.run(part2 = true))
