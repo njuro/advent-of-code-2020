0cacb765217066cdf2c7e7bbaa85fd3f8e7c33ee
njuro <juraj.noge@gmail.com>
2020-12-06 12:53:16 +0100

    Day 6 - one line solution

--- src/main/kotlin/day6.kt
+++ src/main/kotlin/day6.kt
@@ -4,21 +4,10 @@ import utils.readInputBlock
 class Answers : AdventOfCodeTask {
 
     override fun run(part2: Boolean): Any {
-        val groups = readInputBlock("6.txt").split("\n\n")
-        val answers = mutableMapOf<Char, Int>().withDefault { 0 }
-        var counter = 0
-        for (group in groups) {
-            val responses = group.split("\n")
-            for (response in responses) {
-                for (answer in response) {
-                    answers[answer] = answers.getValue(answer) + 1
-                }
-            }
-            counter += if (part2) answers.filterValues { it == responses.size }.size else answers.size
-            answers.clear()
+        return readInputBlock("6.txt").split("\n\n").sumBy { group ->
+            group.split("\n")
+                .map { it.toSet() }.reduce { acc, set -> if (part2) acc.intersect(set) else acc.union(set) }.size
         }
-
-        return counter
     }
 }
 
