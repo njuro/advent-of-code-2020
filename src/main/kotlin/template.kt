ffa728d797f0d309b9cc7b6f6864bfb7b9cadb98
njuro <juraj.noge@gmail.com>
2020-12-09 09:39:55 +0100

    Add template file

---
+++ src/main/kotlin/template.kt
@@ -0,0 +1,14 @@
+import utils.readInputLines
+
+/** [https://adventofcode.com/2020/day/x] */
+class DayX : AdventOfCodeTask {
+    override fun run(part2: Boolean): Any {
+        val input = readInputLines("x.txt")
+
+        return -1
+    }
+}
+
+fun main() {
+    print(DayX().run(part2 = false))
+}
