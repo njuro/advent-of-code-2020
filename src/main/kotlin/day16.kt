b9008ce1eeabdd17277c8b8f7beb140496219459
njuro <juraj.noge@gmail.com>
2020-12-16 08:25:05 +0100

    Day 16

---
+++ src/main/kotlin/day16.kt
@@ -0,0 +1,57 @@
+import utils.readInputBlock
+
+/** [https://adventofcode.com/2020/day/16] */
+class Tickets : AdventOfCodeTask {
+
+    data class Field(val name: String, val range: IntRange, val exceptions: IntRange) {
+        fun isAllowed(value: Int) = value in range && value !in exceptions
+    }
+
+    override fun run(part2: Boolean): Any {
+        val (definitions, your, nearby) = readInputBlock("16.txt").split("\n\n")
+        val definitionPattern = Regex("(.+): (\\d+)-(\\d+) or (\\d+)-(\\d+)")
+
+        val fields = definitions.split("\n").map {
+            val (name, start1, end1, start2, end2) = definitionPattern.matchEntire(it)!!.destructured
+            val allowed = start1.toInt()..end2.toInt()
+            val exceptions = (end1.toInt() + 1) until start2.toInt()
+            Field(name, allowed, exceptions)
+        }
+
+        var errorRate = 0
+        val tickets = parseTickets(nearby).filter { ticket ->
+            ticket.all { value ->
+                val valid = fields.any { field -> field.isAllowed(value) }
+                if (!valid) errorRate += value
+                valid
+            }
+        }
+
+        if (!part2) return errorRate
+
+        val candidates = fields.indices.map { index ->
+            index to fields.filter { field ->
+                tickets.all { ticket -> field.isAllowed(ticket[index]) }
+            }.map(Field::name).toMutableList()
+        }.sortedBy { (_, fields) -> fields.size }
+
+        val determined = mutableSetOf<String>()
+        val valid = candidates.map { (index, fields) ->
+            val field = fields.first { f -> f !in determined }
+            determined.add(field)
+            index to field
+        }.toMap()
+
+        val yourTicket = parseTickets(your).first()
+        return valid.filterValues { it.startsWith("departure") }
+            .keys.fold(1L) { product, index -> product * yourTicket[index] }
+    }
+
+    private fun parseTickets(tickets: String) = tickets.split("\n").drop(1).map { ticket ->
+        ticket.split(",").map { it.toInt() }
+    }
+}
+
+fun main() {
+    print(Tickets().run(part2 = true))
+}
--- src/test/kotlin/AdventOfCodeTasksTest.kt
+++ src/test/kotlin/AdventOfCodeTasksTest.kt
@@ -78,6 +78,11 @@ class AdventOfCodeTasksTest {
         runTaskTest(MemoryGame(), 536, 24065124)
     }
 
+    @Test
+    fun day16() {
+        runTaskTest(Tickets(), 20058, 366871907221L)
+    }
+
     private fun runTaskTest(task: AdventOfCodeTask, part1Result: Any, part2Result: Any) {
         assertEquals(part1Result, task.run())
         assertEquals(part2Result, task.run(part2 = true))
