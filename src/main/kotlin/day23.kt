f0fb2f0e6b5d8bed614d1360e45e96fbdf16f8cc
njuro <juraj.noge@gmail.com>
2020-12-23 08:04:50 +0100

    Day 23 - part 2

--- src/main/kotlin/day23.kt
+++ src/main/kotlin/day23.kt
@@ -1,43 +1,63 @@
+import utils.readInputBlock
+
 /** [https://adventofcode.com/2020/day/23] */
-class Day23 : AdventOfCodeTask {
+class Cups : AdventOfCodeTask {
+
+    data class Node(val value: Int) {
+        lateinit var next: Node
+    }
+
     override fun run(part2: Boolean): Any {
-        var input = "739862541".toCharArray().map { it.toString().toInt() }.toMutableList()
-        var current = input.first()
-        var currentIndex = 0
-        repeat(100) {
-            val moving = listOf(
-                input[(currentIndex + 1) % input.size],
-                input[(currentIndex + 2) % input.size],
-                input[(currentIndex + 3) % input.size]
-            )
-            input.removeAll(moving)
-            var destination = current - 1
-            while (destination !in input) {
-                destination -= 1
-                if (destination < input.minOrNull()!!) {
-                    destination = input.maxOrNull()!!
+        val input = readInputBlock("23.txt").map(Character::getNumericValue)
+        val startNode = Node(input.first())
+        val nodeMap = mutableMapOf(input.first() to startNode)
+        var currentNode = startNode
+        (input.drop(1) + if (part2) 10..1_000_000 else listOf()).forEach {
+            val node = Node(it)
+            nodeMap[it] = node
+            currentNode.next = node
+            currentNode = node
+        }
+        currentNode.next = startNode
+        currentNode = startNode
+
+        repeat(if (part2) 10_000_000 else 100) {
+            val selectedStart = currentNode.next
+            val selectedMiddle = currentNode.next.next
+            val selectedEnd = currentNode.next.next.next
+            val selectedValues = setOf(selectedStart, selectedMiddle, selectedEnd).map(Node::value)
+            currentNode.next = selectedEnd.next
+
+            var destinationValue = currentNode.value
+            do {
+                destinationValue--
+                if (destinationValue == 0) {
+                    destinationValue = if (part2) 1_000_000 else 9
                 }
+            } while (destinationValue in selectedValues)
+            val destinationNode = nodeMap.getValue(destinationValue)
+
+            val destinationNodeNext = destinationNode.next
+            destinationNode.next = selectedStart
+            selectedEnd.next = destinationNodeNext
+            currentNode = currentNode.next
+        }
+
+        val cupOne = nodeMap.getValue(1)
+        return if (part2) {
+            cupOne.next.value.toLong() * cupOne.next.next.value
+        } else {
+            val result = mutableListOf<Int>()
+            var current = cupOne.next
+            while (current.value != 1) {
+                result.add(current.value)
+                current = current.next
             }
-            input.add((input.indexOf(destination) + 1) % input.size, moving[0])
-            input.add((input.indexOf(destination) + 2) % input.size, moving[1])
-            input.add((input.indexOf(destination) + 3) % input.size, moving[2])
-
-            currentIndex = (input.indexOf(current) + 1) % input.size
-            current = input[currentIndex]
+            result.joinToString("").toInt()
         }
-
-        var start = input.indexOf(1)
-        var counter = 1
-        val result = mutableListOf<Int>()
-        while (result.size != input.size - 1) {
-            result.add(input[(start + counter) % input.size])
-            counter++
-        }
-
-        return result.joinToString("")
     }
 }
 
 fun main() {
-    print(Day23().run(part2 = false))
+    print(Cups().run(part2 = false))
 }
--- src/test/kotlin/AdventOfCodeTasksTest.kt
+++ src/test/kotlin/AdventOfCodeTasksTest.kt
@@ -113,6 +113,11 @@ class AdventOfCodeTasksTest {
         runTaskTest(CardGame(), 31809, 32835)
     }
 
+    @Test
+    fun day23() {
+        runTaskTest(Cups(), 94238657, 3072905352L)
+    }
+
     private fun runTaskTest(task: AdventOfCodeTask, part1Result: Any, part2Result: Any) {
         assertEquals(part1Result, task.run())
         assertEquals(part2Result, task.run(part2 = true))
